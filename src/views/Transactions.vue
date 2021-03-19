207256b37c02feec0f78d65fd1f7550114a914eb
Athus Linhares Oliveira <athus.linhares@finxi.com.br>
2021-03-19 02:25:56 -0300

    Criada classe para estilo dos cards de exibição da lista de transações

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
-    <h1>Tokens Emitidos</h1>
-    <div v-for="(item, index) in tokenList" :key="index">
-      <ul>
-        <li>
-          <!-- <p>{{ item.dateTime }}</p> -->
+  <section>
+    <div class="col-1">
+      <h1>Tokens Emitidos</h1>
+      <div v-for="(item, index) in tokenList" :key="index">
+        <div class="card">
           <p>
             <strong>{{ item.fromUserName }}</strong> reconheceu
             {{ item.amount }} tokens E2C para
@@ -14,10 +13,10 @@
             Desejo de acessar de {{ item.toUser }}:
             <strong>{{ item.accessWish }}</strong>
           </p>
-        </li>
-      </ul>
+        </div>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 <script>
 //import { transactions } from '@/firebase'
@@ -26,17 +25,28 @@ export default {
   name: "Transactions",
   data: function() {
     return {
-      tokenList: []
+      tokenList: [],
     };
   },
-  
+
   mounted() {
     this.getTokenList();
   },
   methods: {
     getTokenList() {
-      this.tokenList = this.$store.state.transactions
-    }
-  }
+      this.tokenList = this.$store.state.transactions;
+    },
+  },
 };
 </script>
+<style lang="scss" scoped>
+
+.card{
+  background-color: white;
+  margin-bottom: 5px;
+  margin-top: 5px;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+
+</style>
